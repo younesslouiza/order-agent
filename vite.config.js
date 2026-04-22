78ebefd2c1ead9de49b0f73748950d6698cca75d
younesslouiza <youneslouiza2001@gmail.com>
2026-04-22 12:15:55 +0100

    fix xlsx rolldown

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['xlsx']
-  },
   build: {
+    rollupOptions: {
+      external: [],
+    },
     commonjsOptions: {
       transformMixedEsModules: true,
+    }
+  },
+  resolve: {
+    alias: {}
+  },
+  optimizeDeps: {
+    include: ['xlsx'],
+    esbuildOptions: {
+      target: 'es2020'
     }
   }
 })
